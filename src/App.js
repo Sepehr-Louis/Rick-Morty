5622163023b9d66aea0254b9795355f486d37652
Sepehr louizadeh <sepehrloee@gmail.com>
2023-02-15 15:58:18 +0330

    add first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react';
+import Characters from './Characters';
+import style from './App.css'
 
-function App() {
+
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [search,setSearch] = useState("")
+   useEffect(() => {
+      fetch("https://rickandmortyapi.com/api/character")
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data.results);
+            setPosts(data.results);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+ 
+   const changeHandler = (event) => {
+      setSearch(event.target.value)
+
+   }
+
+   const SerchCharacter = posts.filter(post => post.name.toLowerCase().includes(search.toLowerCase()) )
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+
+    <>
+<input className="search" type="search" placeholder="Search..." value={search} onChange={changeHandler}   />
+
+
+    {
+       SerchCharacter.map(post => <Characters 
+         key={post.id}
+         name={post.name}
+         species={post.species}
+         status={post.status}
+         image={post.image}
+       
+       
+       
+       
+       
+       />)
+    }
+   
+    
+       
+    </>
   );
-}
+};
 
 export default App;
